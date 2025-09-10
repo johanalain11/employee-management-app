290483d6cb004fe66b0b6cbeede0fc700768ef69
johanalain11 <jank092016@gmail.com>
2025-09-10 13:52:28 +0100

    Add layout, navbar, dashboard, employee, home, leaves, and login components + employee service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './components/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Employee } from './pages/employee/employee';
+import { Leaves } from './pages/leaves/leaves';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: Home
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path:'',
+    component:Layout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard
+      },
+      {
+        path: 'employee',
+        component: Employee
+      },
+      {
+        path: 'leaves',
+        component: Leaves
+      }
+    ]
+  }
+];
