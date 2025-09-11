9d05524a2f82ee8976cb337c6809c27ba657b914
johanalain11 <jank092016@gmail.com>
2025-09-11 14:39:31 +0100

    Add Hero component with styles and integrate into Home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
+import { Hero } from '../../components/hero/hero'
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, Hero],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
