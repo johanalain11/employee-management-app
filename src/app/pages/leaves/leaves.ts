6e23f5d977fdd2efab38f40dc2b207ca4c7bec44
johanalain11 <jank092016@gmail.com>
2025-10-16 17:38:26 +0100

    Add leave management modal and form functionality: implement modal for adding leave requests, integrate reactive forms, and enhance UI with DaisyUI tabs

--- src/app/pages/leaves/leaves.ts
+++ src/app/pages/leaves/leaves.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-leaves',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './leaves.html',
   styleUrl: './leaves.css'
 })
 export class Leaves {
+
+  @ViewChild("newModal") newModal!: ElementRef;
+
+  leaveForm: FormGroup = new FormGroup({
+    leaveId: new FormControl(0),
+    employeeId: new FormControl(0),
+    fromDate: new FormControl(''),
+    toDate: new FormControl(''),
+    noOfDays: new FormControl(''),
+    leaveType: new FormControl(''),
+    details: new FormControl(''),
+    isApproved: new FormControl(false),
+    approvedDate: new FormControl(null)
+  })
+
+  openModal() {
+    if(this.newModal) {
+      this.newModal.nativeElement.style.display = "block";
+    }
+  }
+
+  closeModal() {
+    if(this.newModal) {
+      this.newModal.nativeElement.style.display = "none";
+    }
+  }
 
 }
