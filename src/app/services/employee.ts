ed6955701ca96153d36b44472918bf830166ba2d
johanalain11 <jank092016@gmail.com>
2025-11-03 14:52:22 +0000

    Update Employee service

--- src/app/services/employee.ts
+++ src/app/services/employee.ts
@@ -19,13 +19,6 @@ export class EmployeeService {
     return this.http.get<APIResponseModel>(`${this.baseUrl}/GetEmployees`);
   }
 
-  getEmployeesNumber() {
-    let employees: any = this.http.get(`${this.baseUrl}/GetEmployeesNumber`).pipe(
-      map((response: any) => response.data)
-    );
-    return employees.length;
-  }
-
   getDepartments() {
     return this.http.get(`${this.baseUrl}/GetDepartments`).pipe(
       map((response: any) => response.data)
