91b5aad4597fb6c36ce3ad942aeefcc3b2bb4298
johanalain11 <jank092016@gmail.com>
2025-11-04 16:08:05 +0000

    Update dashboard page

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeModel, APIResponseModel } from '../../models/employeeModel';
+import { EmployeeService } from '../../services/employee';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit{
 
+  employees: EmployeeModel[] = [];
+  employeeCount: number = 0;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  getEmployees() : void {
+    this.employeeService.getEmployees().subscribe({
+      next: (response: APIResponseModel) => {
+        this.employees = response.data;
+      },
+      error: () => {
+        alert('Error while fetching the records!!');
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.getEmployees();
+    this.employeeCount = this.employees.length;
+  }
 }
