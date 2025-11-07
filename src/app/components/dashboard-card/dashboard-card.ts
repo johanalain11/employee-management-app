feb5cc627e41f356c85b7ce071f62c33a62d6310
johanalain11 <jank092016@gmail.com>
2025-11-07 15:31:10 +0000

    Update dashboard cards by adding a random background

--- src/app/components/dashboard-card/dashboard-card.ts
+++ src/app/components/dashboard-card/dashboard-card.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-card.html',
   styleUrl: './dashboard-card.css'
 })
-export class DashboardCard {
+export class DashboardCard implements OnInit{
 
   @Input() name: string = '';
   @Input() count: number = 0;
+
+  bgColor: string = '';
+
+  ngOnInit() {
+    this.bgColor = this.getRandomColor();
+  }
+
+  private getRandomColor(): string {
+    const hue = 200 + Math.random() * 40;     // teinte entre 200° et 240°
+    const saturation = 60 + Math.random() * 30; // saturation entre 60–90%
+    const lightness = 30 + Math.random() * 40;  // luminosité entre 30–70%
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }
 }
