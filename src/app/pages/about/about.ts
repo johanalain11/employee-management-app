fc33b0cbf805a49ac7942bd250cc0986c7fea49f
johanalain11 <jank092016@gmail.com>
2025-11-17 16:57:49 +0000

    Update About page

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, HostListener, AfterViewInit, ElementRef } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +8,37 @@ import { Navbar } from '../../components/navbar/navbar';
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
-export class About {
+export class About implements AfterViewInit {
+
+  private router = inject(Router)
+
+  constructor(private el: ElementRef) {}
+
+  signin() {
+    this.router.navigate(['./login']);
+  }
+
+  ngAfterViewInit(): void {
+    this.checkAnimations();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkAnimations();
+  }
+
+  private checkAnimations() {
+    const items = this.el.nativeElement.querySelectorAll('.animatable');
+
+    items.forEach((element: HTMLElement) => {
+      const rect = element.getBoundingClientRect();
+      const inView = rect.top + rect.height - 20 < window.innerHeight;
+
+      if (inView && !element.classList.contains('animated')) {
+        element.classList.add('animated');
+        element.classList.remove('animatable');
+      }
+    });
+  }
 
 }
